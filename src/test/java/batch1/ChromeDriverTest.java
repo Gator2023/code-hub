ecf4aeb785e0f93f6fcd9da3e4afadf765725b9b
Salma <salmahossain52@gmail.com>
2023-02-10 11:53:38 -0600

    https://github.com/Gator2023/SeleniumProject

--- src/test/java/batch1/ChromeDriverTest.java
+++ src/test/java/batch1/ChromeDriverTest.java
@@ -9,7 +9,7 @@ public class ChromeDriverTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
-        driver.get("https://www.amazon.com/");
+        driver.get("https://money.rediff.com/companies/Tata-Consultancy-Services-Ltd/13020033?query=Tcs");
         Thread.sleep(3000);
         driver.quit();
 
